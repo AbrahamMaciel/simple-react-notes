81659c6ef2a72dd1cf0d0819acd08e294f9a0858
Abraham Maciel <fernandezabmaciel@gmail.com>
2024-11-21 01:19:08 -0300

    Added a simple markdown editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./Editor.css";
+import MDEditor, { commands } from "@uiw/react-md-editor";
 
 function Editor() {
+  const [value, setValue] = React.useState("");
   return (
-    <section className='editor'>
-        <h2>Editor</h2>
+    <section className="editor" data-color-mode="light">
+      <MDEditor
+        value={value}
+        onChange={setValue}
+        height="100%"
+        visibleDragbar={false}
+        textareaProps={{
+          placeholder: "Please do some shit",
+        }}
+        commands={[
+          commands.comment,
+          commands.bold,
+          commands.italic,
+          commands.strikethrough,
+          commands.hr,
+          commands.title,
+          commands.divider,
+          commands.link,
+          commands.quote,
+          commands.divider,
+          commands.unorderedListCommand,
+          commands.orderedListCommand,
+          commands.checkedListCommand,
+          commands.help,
+        ]}
+      />
     </section>
-  )
+  );
 }
 
-export default Editor
+export default Editor;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,12 +6,20 @@ function Sidebar() {
     <section className="sidebar">
       <div className="sidebar--header">
         <h2>Notes</h2>
-        <button type="button">+</button>
+        <button type="button" class="sidebar--button">
+          +
+        </button>
       </div>
       <div className="sidebar--notes">
-        <div className="sidebar--note">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
-        <div className="sidebar--note">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
-        <div className="sidebar--note">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
+        <div className="sidebar--note">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </div>
+        <div className="sidebar--note">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </div>
+        <div className="sidebar--note">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </div>
       </div>
     </section>
   );
